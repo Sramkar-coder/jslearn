6dfb6c31bc95d313451dfe6f7baefe347ff29990
sramkar <sramkar@bitbucket.org>
2015-04-08 20:43:41 -0400

    prototypal model

---
+++ prototypalmodel.js
@@ -0,0 +1,23 @@
+var Vehicle = function(VIN, color, naxels){
+        this.VIN = VIN;
+        this.color = color;
+        this.naxels = naxels;
+        this.speed = 0;
+};
+
+Vehicle.prototype.setSpeed = function(x){
+        this.speed = x;
+}
+
+
+Vehicle.prototype.getSpeed = function(x){
+        return this.speed;
+};
+
+
+
+var myCar = new Vehicle("324AEAWEQWE4234", "black", 4);
+console.log(myCar.getSpeed());
+
+
+
